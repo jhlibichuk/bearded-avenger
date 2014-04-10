f4dc9d4185257b4fe6728e1ae998c7fcb740b5f3
josh <josh@josh-VirtualBox.(none)>
2014-04-09 23:14:01 -0500

    Adding rails initialized code

---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+BeardedAvenger::Application.config.session_store :cookie_store, key: '_bearded-avenger_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# BeardedAvenger::Application.config.session_store :active_record_store
