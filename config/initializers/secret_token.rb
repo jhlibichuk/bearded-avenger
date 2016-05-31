1ba6c1c2bebe1f5fd64694493fb11f880d3e4fc3
Josh Hlibichuk <josh@fusionfarm.com>
2016-05-30 20:30:03 -0500

    fixed untracked files

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Snapsplore::Application.config.secret_token = '62c4421eeeefca3c95c642985c549202ddb5955d9c3df1a220bdd5ccd8eda16d4ca9b50522e7e22c560d5b0bad05e21e7b7482100e8455360341fb7097f9632b'
